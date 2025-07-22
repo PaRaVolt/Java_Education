95089165b4a335e60ffe73e6b57d9011b580db74
PaRaVolt <gt_news@mail.ru>
2025-07-22 14:05:52 +0500

    Домашнее задание 1. Hello ...

--- HelloWorld/src/main/java/ru/java/education/dz1/Main.java
+++ HelloWorld/src/main/java/ru/java/education/dz1/Main.java
@@ -1,8 +1,14 @@
 package ru.java.education.dz1;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args)
     {
-        System.out.println("Привет, Имя!");
+        Scanner src = new Scanner(System.in);
+
+        System.out.println("Привет, " + src.next() + "!");
+
+        src.close();
     }
 }
